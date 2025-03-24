641dcc7e9f5ee16e579f4af62d6e954a2dad6ecb
kenchanta <exnowman@gmail.com>
2025-03-24 21:13:52 +0900

    Lesson15　再提出1回目

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class OutputController {
+
     @PostMapping("/output")
     public String postOutput(@RequestParam("val") String val, Model model){
         // フォームから送信されてきた値をModelに登録
